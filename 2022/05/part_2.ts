80264b7344d6579165b113eb65ed7de396c5bd83
JuanM04 <me@juanm04.com>
2022-12-05 11:29:50 -0300

    Day 5

---
+++ 2022/05/part_2.ts
@@ -0,0 +1,32 @@
+const input = await Deno.readTextFile(
+  new URL(import.meta.resolve("./input.txt"))
+);
+const [initialConditions, instructions] = input
+  .split("\n\n")
+  .map((block) => block.split("\n"));
+
+const stack = new Array(initialConditions.at(-2)!.split(" ").length)
+  .fill(undefined)
+  .map(() => [] as string[]);
+
+for (let i = initialConditions.length - 2; i >= 0; i--) {
+  const line = initialConditions[i];
+  for (let j = 0; j < stack.length; j++) {
+    const crate = line.slice(j * 4 + 1, j * 4 + 2);
+    if (crate !== " ") stack[j].push(crate);
+  }
+}
+
+const instructionRegex = /^move (?<len>\d+) from (?<from>\d+) to (?<to>\d+)$/;
+for (const instruction of instructions) {
+  const parsed = instruction.match(instructionRegex)!.groups!;
+  const len = parseInt(parsed.len);
+  const from = parseInt(parsed.from) - 1;
+  const to = parseInt(parsed.to) - 1;
+
+  stack[to].push(...stack[from].splice(-len));
+}
+
+const topCrates = stack.map((crates) => crates.at(-1)!).join("");
+
+console.log(topCrates);
